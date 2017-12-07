e8bd2ccf63ef58b7864ba2f717637da3f5ecb741
vinod10 <vinodkumar@kwiqpick.com>
2017-12-07 11:58:23 +0530

    first graphql query

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,13 +3,27 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component{
+
+    renderSongs(){
+        return this.props.data.songs.map(song => {
+            return (
+                <li key={song.id} className="collection-item">
+                    {song.title}
+                </li>
+            );
+        })
+    }
+
     render(){
 
-        console.log(this.props);
+        if (this.props.data.loading){
+            return <div>Loading...</div>
+        }
+        //console.log(this.props);
         return(
-            <div>
-                SongList
-            </div>
+            <ul className="collection">
+                {this.renderSongs()}
+            </ul>
         );
     }
 }
@@ -17,6 +31,7 @@ class SongList extends Component{
 const query = gql`
     {
         songs{
+            id
             title
         }
     }
--- client/index.js
+++ client/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
-import {ApolloProvider} from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
 
+import App from './components/App';
+import SongCreate from './components/SongCreate';
 import SongList from './components/SongList';
 
 const client = new ApolloClient({});
@@ -10,9 +13,12 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
       <ApolloProvider client={client}>
-        <div>
-          <SongList/>
-        </div>
+        <Router history={hashHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={SongList}/>
+                <Route path="song/new" component={SongCreate}/>
+            </Route>
+        </Router>
       </ApolloProvider>
   );
 };
